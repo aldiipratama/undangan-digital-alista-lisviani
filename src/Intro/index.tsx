32dff31bb2ececed25321b8de1fe4d8109007e76
aldiipratama <paldi0013@gmail.com>
2026-04-24 19:14:57 +0700

    fixing variants easing error

--- src/Intro/index.tsx
+++ src/Intro/index.tsx
@@ -29,7 +29,7 @@ export default function Intro() {
       y: 0,
       transition: {
         duration: 1,
-        ease: [0.22, 1, 0.36, 1],
+        ease: [0.22, 1, 0.36, 1] as const,
       },
     },
   };
